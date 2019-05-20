a413aac0b581bdfb5a097684864a6a74dc3ba387
schazbot <charlotte.pearce1984@gmail.com>
2019-05-20 17:14:02 +0100

    updating picture text achieved

--- src/components/PictureCardModal.js
+++ src/components/PictureCardModal.js
@@ -5,27 +5,39 @@ const PictureCardModal = props => {
   const { picture } = props;
 
   return (
-    <Card
-      key={props.picture.id}
-      onClick={() => props.handleClick(picture)}
-      wrapped
-      ui={false}
-    >
+    <Card key={picture.id} wrapped >
       <Card.Content>
         <Image src={picture.url} />
         <Card.Description>{picture.text.toLowerCase()}</Card.Description>
       </Card.Content>
 
-      <Modal size={"small"} trigger={<Button color={"blue"}>Edit</Button>}>
+      <Modal
+        size={"small"}
+        trigger={
+          <Button
+            onClick={() => {
+              props.setPicture(picture);
+            }}
+            color={"blue"}
+          >
+            Edit
+          </Button>
+        }
+      >
         <Modal.Header>Edit Text</Modal.Header>
         <Modal.Content image>
           <Image wrapped size="medium" src={picture.url} />
           <Form>
             <Form.Field>
               <label>Type to change text</label>
-              <input placeholder={picture.text} />
+              <input
+                onChange={props.handlePictureFormChange}
+                placeholder={picture.text}
+              />
             </Form.Field>
-            <Button color={"green"}>Save</Button>
+            <Button onClick={props.editPicture} color={"green"}>
+              Save
+            </Button>
 
             <Button color={"red"}>Delete</Button>
             <Label size={"big"}>Delete picture from folder</Label>
